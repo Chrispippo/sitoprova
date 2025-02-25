35d2870b2fb82c99e4f02550a0de116a0d2bc1a4
Chrispippo <netbergamini@gmail.com>
2025-02-25 15:55:34 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Il sito è stato caricato correttamente!');
+  // Puoi inserire qui il codice per le chiamate API o altre interazioni
+});
