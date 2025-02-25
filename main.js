c7e767abb4c384fca9d527e22b8b3b0971e3daf7
Chrispippo <netbergamini@gmail.com>
2025-02-25 16:07:11 +0100

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Il sito è stato caricato correttamente!');
-  // Puoi inserire qui il codice per le chiamate API o altre interazioni
-});
